56ff04fba78b6bd1e19ec27febee444cc5ce1206
David Gillard <consult@davidgillard.com>
2022-12-01 23:09:47 -0330

    day one complete

---
+++ AdventOfCode/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open System.IO
+open AdventOfCode.Solutions
+
+let day = Environment.GetCommandLineArgs()[1]
+let problem = Environment.GetCommandLineArgs()[2]
+
+let solution =
+  match day, problem with
+  | "1", "1" -> ``dayOne.1``
+  | "1", "2" -> ``dayOne.2``
+  | unknownInput -> failwithf $"Unknown input {unknownInput}"
+
+let inputFile = $"day{day}.txt"
+let stream = new StreamReader(inputFile)
+printfn $"{solution stream}"
+stream.Dispose()
---
+++ AdventOfCode/Solutions.fs
@@ -0,0 +1,46 @@
+﻿module AdventOfCode.Solutions
+
+open System
+open System.IO
+
+let streamToLines (inputStream: StreamReader) =
+  let rec getLines (s: StreamReader) =
+    seq {
+      let line = inputStream.ReadLine()
+      if line <> null then
+        yield line
+        yield! getLines s
+    }
+  getLines inputStream
+    
+let ``dayOne.1`` (inputStream: StreamReader) = 
+  Seq.foldBack (fun (line: string) (ll: int list list) ->
+                    let mutable result = 0
+                    if Int32.TryParse(line, &result) then
+                      (result::ll.Head)::ll.Tail
+                    else
+                      []::ll
+                  )
+                  (streamToLines inputStream)
+                  [[]]
+  |> List.map List.sum
+  |> List.max
+  
+let ``dayOne.2`` (inputStream: StreamReader) = 
+  Seq.foldBack (fun (line: string) (ll: int list list) ->
+                    let mutable result = 0
+                    if Int32.TryParse(line, &result) then
+                      (result::ll.Head)::ll.Tail
+                    else
+                      []::ll
+                  )
+                  (streamToLines inputStream)
+                  [[]]
+  |> List.map List.sum
+  |> List.sort
+  |> List.rev
+  |> List.take 3
+  |> List.sum
+
+
+
